ebaf43c4bc174946cc4012e3820bcca9c448d98c
tanurehal <72433281+tanurehal@users.noreply.github.com>
2021-06-27 07:38:14 +0530

    Add files via upload

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from "react";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+    return (<Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton  onClick={props.onShowCart}/>
+        </header>
+        <div className={classes.mainimage}>
+            <img src={mealsImage} alt='header-img'/>
+        </div>
+    </Fragment>);
+};
+
+export default Header;
